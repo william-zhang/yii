27629211eff36cd1d1f0dbac87f5d9b1be69c635
william-zhang <williamzhang1987@gmail.com>
2013-07-21 17:46:52 +1000

    trail

---
+++ demos/blog/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// change the following paths if necessary
+date_default_timezone_set('America/Los_Angeles');
+$yii=dirname(__FILE__).'/../../framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+
+// remove the following line when in production mode
+// defined('YII_DEBUG') or define('YII_DEBUG',true);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
